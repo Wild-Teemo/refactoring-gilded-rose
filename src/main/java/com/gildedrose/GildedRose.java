961ef6cbe42e0f611480cb1cc7e00689556e3a84
MengyaoLin <mengyao.lin@thoughtworks.com>
2020-03-02 12:25:20 +0800

    extract private functions to calculate quality and sell in GildedRose

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -70,4 +70,24 @@ class GildedRose {
     }
   }
 
+  private void updatedBackstageRoseQuality(Item item) {
+    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (item.sell_in < 11) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+
+      if (item.sell_in < 6) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+
+      if (item.sell_in < 0) {
+        item.quality = item.quality - item.quality;
+      }
+    }
+  }
+
 }
