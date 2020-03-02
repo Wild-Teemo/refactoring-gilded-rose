55f75ce45061b109e6987efab64e599de719d149
MengyaoLin <mengyao.lin@thoughtworks.com>
2020-03-02 13:10:01 +0800

    extract constant.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,7 +2,11 @@ package com.gildedrose;
 
 class GildedRose {
 
-  Item[] items;
+  private static final String AGED_BRIE_ROSE = "Aged Brie";
+  private static final String BACKSTAGE_PASSES_ROSE = "Backstage passes to a TAFKAL80ETC concert";
+  private static final String SULFURAS_ROSE = "Sulfuras, Hand of Ragnaros";
+
+  public Item[] items;
 
   public GildedRose(Item[] items) {
     this.items = items;
@@ -10,16 +14,16 @@ class GildedRose {
 
   public void update_quality() {
     for (Item item : items) {
-      updatedSellIn(item);
-      updatedQuality(item);
+      updateSellIn(item);
+      updateQuality(item);
     }
   }
 
-  private void updatedQuality(Item item) {
+  private void updateQuality(Item item) {
     switch (item.name) {
-      case "Sulfuras, Hand of Ragnaros":
+      case SULFURAS_ROSE:
         return;
-      case "Aged Brie":
+      case AGED_BRIE_ROSE:
         if (item.sell_in < 0 && item.quality < 50) {
           item.quality = item.quality + 1;
         }
@@ -27,11 +31,11 @@ class GildedRose {
           item.quality = item.quality + 1;
         }
         return;
-      case "Backstage passes to a TAFKAL80ETC concert":
+      case BACKSTAGE_PASSES_ROSE:
         if (item.quality < 50) {
           item.quality = item.quality + 1;
         }
-        updatedBackstageRoseQuality(item);
+        updateBackstageRoseQuality(item);
         return;
       default:
         if (item.quality > 0) {
@@ -43,7 +47,7 @@ class GildedRose {
     }
   }
 
-  private void updatedBackstageRoseQuality(Item item) {
+  private void updateBackstageRoseQuality(Item item) {
     if (item.sell_in < 6) {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
@@ -61,8 +65,8 @@ class GildedRose {
     }
   }
 
-  private void updatedSellIn(Item item) {
-    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+  private void updateSellIn(Item item) {
+    if (!item.name.equals(SULFURAS_ROSE)) {
       item.sell_in = item.sell_in - 1;
     }
   }
